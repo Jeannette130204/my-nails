a148b15c407a3e3291f96df4e6742f6ddaa6dd3c
Jeannette130204 <jeannette130204@gmail.com>
2023-03-09 23:05:51 -0800

    Add names and change css

--- src/components/NailCard.jsx
+++ src/components/NailCard.jsx
@@ -2,6 +2,7 @@ export default function NailCard({ nail }){
     return (
         <div id="one-pic">
             <img src={`${nail.picture}`} id="nail-pics"/>
+            <p><strong>{ nail.name } </strong></p>
         </div>
     )
 
